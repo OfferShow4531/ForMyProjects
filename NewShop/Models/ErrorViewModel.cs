f2eb687841c1e257c93e5ddbcd4c7aca3d2efcdd
Timur Em <offershow4531@gmail.com>
2020-07-01 14:00:49 +0600

    first project commit

---
+++ NewShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ NewShop/Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace NewShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public decimal ProductCost { get; set; }
+        public decimal ProductVallue { get; set; }
+        public decimal DollarChange { get; set; }
+        public decimal TengeChange { get; set; }
+        public decimal EuroChange { get; set; }
+        
+    }
+}
